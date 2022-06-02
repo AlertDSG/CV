d92e64b52394e7a4a559ced6cc60c3f84a505493
dima <AlertDSG@gmail.com>
2022-06-03 01:02:13 +0300

    Add component AboutMe and some style

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from 'react'
+import style from './AboutMe.module.css'
 
 export const AboutMe = () => {
     return (
-        <div>
+        <section className={style.aboutMe}>
+            <div className={'container'}>
+                <div className={style.contentBlock}>
+                    <div className={style.text}>
+                        <span>Hi There</span>
+                        <h1>My name is Dmitryi</h1>
+                        <span>Frontend Developer</span>
+                    </div>
+                    <div className={style.photoBlock}>
+                        <img className={style.photo} src="https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80" alt="my photo"/>
+                    </div>
+                </div>
+            </div>
 
-        </div>
+        </section>
     );
 };
 
