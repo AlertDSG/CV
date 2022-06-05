a23a898efb68d1cf53209de95333d936dc9550db
dima <AlertDSG@gmail.com>
2022-06-05 19:25:15 +0300

    Add component Footer and some styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import style from './Footer.module.css';
 
 export const Footer = () => {
     return (
-        <footer>
-
+        <footer className={style.footerBlock}>
+            <div className={'container'}>
+                <div className={style.contentBlock}>
+                    <h2 className={style.title}>Slabko Dmitryi</h2>
+                    <div className={style.cardBlock}>
+                        <div className={style.item}></div>
+                        <div className={style.item}></div>
+                        <div className={style.item}></div>
+                        <div className={style.item}></div>
+                    </div>
+                    <span>&copy; 2022</span>
+                </div>
+            </div>
         </footer>
     );
 };
