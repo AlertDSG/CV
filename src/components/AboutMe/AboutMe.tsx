a8c4ea64e2b767c463af5b4fd79fca9ac5cd44dd
dima <AlertDSG@gmail.com>
2022-09-03 01:31:11 +0300

    add parallax

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,32 +1,14 @@
 import React from 'react'
 import style from './AboutMe.module.scss'
+import Tilt from 'react-parallax-tilt';
 
-
-
-// import Particles from "react-particles-js";
-//
-//
-//
-// const particlesOpt = {
-//     "particles": {
-//         "number": {
-//             "value": 150,
-//             "density": {
-//                 "enable": true,
-//                 "value_area": 800
-//             },
-//         },
-//     },
-// }
 type AboutType = {
     positionHandler: (status: string) => void
 }
 export const AboutMe = ({positionHandler}: AboutType) => {
 
-
     return (
         <section id={'about'} className={style.aboutMe} onWheel={event => positionHandler(event.currentTarget.id)}>
-            {/*<Particles />*/}
             <div className={'container'}>
                 <div className={style.contentBlock}>
                     <div className={style.text}>
@@ -34,8 +16,10 @@ export const AboutMe = ({positionHandler}: AboutType) => {
                         <h1 className={style.allName}>My name is <span className={style.name}>DZMITRIY,</span></h1>
                         <span className={style.anyText}>And I'm Frontend Developer</span>
                     </div>
-                    <div className={style.photoBlock}>
-                    </div>
+                    <Tilt glareReverse={true}>
+                        <div className={style.photoBlock}>
+                        </div>
+                    </Tilt>
                 </div>
             </div>
         </section>
